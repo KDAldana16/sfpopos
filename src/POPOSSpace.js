fb3d6782ad21ac5e0137fe3649536dd60f62f204
Karla Aldana <karla.aldana@students.dominican.edu>
2020-04-11 16:14:56 -0700

    fixed the names and addresses

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -8,8 +8,8 @@ function POPOSSpace(props) {
   return (
     <div>
       <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
-      <h1>(name).</h1>
-      <div>(address)</div>
+      <h1>{name}.</h1>
+      <div>{address}</div>
     </div>
   )
 }
