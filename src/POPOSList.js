9eb0099d7ef21b5408fd13982815b15ceadedb82
Karla Aldana <karla.aldana@students.dominican.edu>
2020-04-11 16:03:38 -0700

    still figuring it out

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -11,32 +11,32 @@ function POPOSList() {
       <POPOSSpace
         name="50 California Street"
         address="50 California St."
-        image="images/50-california-st.jpg"
+        image="50-california-st.jpg"
       />
       <POPOSSpace
         name="100 Pine Street"
         address="100 Pine St."
-        image="images/100-pine.jpg"
+        image="100-pine.jpg"
       />
       <POPOSSpace
         name="101 California Street"
         address="101 California"
-        image="images/101-california.jpg"
+        image="101-california.jpg"
       />
       <POPOSSpace
         name="343 Sansome Roof Garden"
         address="343 Sansome St."
-        image="images/343-sansome-roof-garden.jpg"
+        image="343-sansome-roof-garden.jpg"
       />
       <POPOSSpace
         name="525 Market Street Plaza"
         address="525 Market St."
-        image="images/525-market-street-plaza.jpg"
+        image="525-market-street-plaza.jpg"
       />
       <POPOSSpace
         name="Citigroup Center"
         address="1 Sansome St."
-        image="images/citigroup-center.jpg"
+        image="citigroup-center.jpg"
       />
     </div>
   )
