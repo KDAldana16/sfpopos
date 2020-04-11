cf845998616661834cf9769dad999e7d05adecfe
Karla Aldana <karla.aldana@students.dominican.edu>
2020-04-11 15:58:51 -0700

    trying to make the images work

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -1,6 +1,7 @@
 
 // src/POPOSList.js
 
+import './POPOSList.css';
 import React from 'react';
 import POPOSSpace from './POPOSSpace';
 
@@ -8,34 +9,34 @@ function POPOSList() {
   return (
     <div className="POPOSList">
       <POPOSSpace
-          name="50 California Street"
-          address="50 California St."
-          image="50-california-st.jpg"
+        name="50 California Street"
+        address="50 California St."
+        image="images/50-california-st.jpg"
       />
       <POPOSSpace
-          name="100 Pine Street"
-          address="100 Pine St."
-          image="100-pine.jpg"
+        name="100 Pine Street"
+        address="100 Pine St."
+        image="images/100-pine.jpg"
       />
       <POPOSSpace
-          name="101 California Street"
-          address="101 California"
-          image="101-california.jpg"
+        name="101 California Street"
+        address="101 California"
+        image="images/101-california.jpg"
       />
       <POPOSSpace
-          name="343 Sansome Roof Garden"
-          address="343 Sansome St."
-          image="343-sansome-roof-garden.jpg"
+        name="343 Sansome Roof Garden"
+        address="343 Sansome St."
+        image="images/343-sansome-roof-garden.jpg"
       />
       <POPOSSpace
-          name="525 Market Street Plaza"
-          address="525 Market St."
-          image="525-market-street-plaza.jpg"
+        name="525 Market Street Plaza"
+        address="525 Market St."
+        image="images/525-market-street-plaza.jpg"
       />
       <POPOSSpace
-          name="Citigroup Center"
-          address="1 Sansom St."
-          image="citigroup-center.jpg"
+        name="Citigroup Center"
+        address="1 Sansome St."
+        image="images/citigroup-center.jpg"
       />
     </div>
   )
